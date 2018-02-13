c1edf6e019b3b8f850c5883bfda7e05875a04627
Chaofan Zhang <chaofan.zhang@ef.com>
2018-02-13 16:58:14 +0800

    Static Caching/Precaching and Retrieving the App Shell.

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,27 @@
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
+    // waitUntil() method tells the event dispatcher that work is ongoing.
+    // It can also be used to detect whether that work was successful.
+    // In service workers, waitUntil() tells the browser that work is ongoing 
+    // until the promise settles, and it shouldn't terminate the service worker
+    // if it wants that work to complete.
+    event.waitUntil(caches.open('static')
+                        .then(function(cache) {
+                            console.log('[Service Worker] Precaching App Shell');
+                            cache.addAll([
+                                '/',
+                                '/index.html',
+                                '/src/js/app.js',
+                                '/src/js/feed.js',
+                                '/src/js/material.min.js',
+                                '/src/css/app.css',
+                                '/src/css/feed.css',
+                                '/src/images/main-image.jpg',
+                                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                                'https://fonts.googleapis.com/icon?family=Material+Icons',
+                                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                            ])
+                        }));
 });
 
 self.addEventListener('activate', function(event) {
@@ -8,5 +30,14 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    );
 })
